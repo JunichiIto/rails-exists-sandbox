c25b22d679e420bc440ed8192eb4d130c83152b4
Junichi Ito <jit@sonicgarden.jp>
2021-03-03 19:41:40 +0900

    Add sample code

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,3 +1,19 @@
 class Parent < ApplicationRecord
   has_many :children
+
+  # scope :children_name_with, -> (str) do
+  #   left_outer_joins(:children).where("LOWER(children.name) LIKE ?", "%#{str.downcase}%").distinct
+  # end
+
+  scope :children_name_with, -> (str) do
+    sql = <<~SQL
+      EXISTS(
+        SELECT *
+        FROM children c
+        WHERE c.parent_id = parents.id
+        AND LOWER(c.name) LIKE ?
+      )
+    SQL
+    where(sql, "%#{str}%")
+  end
 end
--- test/models/parent_test.rb
+++ test/models/parent_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class ParentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test ".children_name_with" do
+    parents = Parent.children_name_with('z').order(:name)
+    assert_equal [parents(:namihei)], parents
+
+    parents = Parent.children_name_with('k').order(:name)
+    assert_equal [parents(:misae), parents(:namihei)], parents
+
+    parents = Parent.children_name_with('r').order(:name)
+    assert_equal [parents(:misae)], parents
+  end
 end
