75eb21c861d4728fac12cd93d1b7d3c381b5b0b1
Junichi Ito <jit@sonicgarden.jp>
2021-03-04 08:18:05 +0900

    Refactor tests

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -2,18 +2,23 @@ class Parent < ApplicationRecord
   has_many :children
 
   # scope :children_name_with, -> (str) do
-  #   left_outer_joins(:children).where("LOWER(children.name) LIKE ?", "%#{str.downcase}%").distinct
+  #   joins(:children).where("LOWER(children.name) LIKE ?", "%#{str.downcase}%").distinct
   # end
 
   scope :children_name_with, -> (str) do
-    sql = <<~SQL
-      EXISTS(
-        SELECT *
-        FROM children c
-        WHERE c.parent_id = parents.id
-        AND LOWER(c.name) LIKE ?
-      )
-    SQL
-    where(sql, "%#{str}%")
+    ids = Parent.joins(:children).where("LOWER(children.name) LIKE ?", "%#{str.downcase}%").select(:id)
+    where(id: ids)
   end
+
+  # scope :children_name_with, -> (str) do
+  #   sql = <<~SQL
+  #     EXISTS(
+  #       SELECT *
+  #       FROM children c
+  #       WHERE c.parent_id = parents.id
+  #       AND LOWER(c.name) LIKE ?
+  #     )
+  #   SQL
+  #   where(sql, "%#{str}%")
+  # end
 end
--- test/models/parent_test.rb
+++ test/models/parent_test.rb
@@ -1,13 +1,20 @@
 require "test_helper"
 
 class ParentTest < ActiveSupport::TestCase
-  test ".children_name_with" do
+  test "zで検索" do
+    # Sazaeが該当するのでNamiheiが返る
     parents = Parent.children_name_with('z').order(:name)
     assert_equal [parents(:namihei)], parents
+  end
 
+  test "kで検索" do
+    # ShinnosukeとKatsuoとWakameが該当するのでMisaeとNamiheiが返る
     parents = Parent.children_name_with('k').order(:name)
     assert_equal [parents(:misae), parents(:namihei)], parents
+  end
 
+  test "rで検索" do
+    # Himawariが該当するのでMisaeが返る
     parents = Parent.children_name_with('r').order(:name)
     assert_equal [parents(:misae)], parents
   end
